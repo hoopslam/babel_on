18977730d85b1e154d9c8f5b19e8ab3fccd84ce0
David <hoopslam@gmail.com>
2023-03-23 14:00:36 +0900

    update: adjust ai prompt

--- util/openai.ts
+++ util/openai.ts
@@ -12,14 +12,13 @@ export const queryOpenAI = async (
     learningLanguage: string,
     nativeLanguage: string
 ) => {
-    console.log(learningLanguage);
     try {
         const completion = await openai.createChatCompletion({
             model: `gpt-3.5-turbo`,
             messages: [
                 {
                     role: 'system',
-                    content: `You are a ${learningLanguage} language teacher.  The user is your student and ${nativeLanguage} is his native language. You are teaching a conversation class based on a speaking topic.  You can either suggest a topic or ask the user what he would like to talk about.  You always respond in ${learningLanguage} but also provide the ${nativeLanguage} translation.  You do not have to provide a romanization. If the user replies in ${nativeLanguage}, provide the ${learningLanguage} version of what the user wrote and encourage him to try to use ${learningLanguage} when writing a response.  Try to correct some of the mistakes your student might make. Try to be humorous. You should ask lots of questions in ${learningLanguage} to stimulate conversation.`,
+                    content: `You are a ${learningLanguage} language teacher.  The user is your student and ${nativeLanguage} is his native language. You are teaching a conversation class based on a random speaking topic that you can suggest.  In additio, the student can choose his own topic to talk about.  You always respond in ${learningLanguage} but also provide the ${nativeLanguage} translation. If the user replies in ${nativeLanguage}, provide the ${learningLanguage} version of what the user wrote.  Try to correct any grammar, spelling, or word choice mistakes your student might make. You should always end your responses with a follow up question to keep the conversation going.`,
                 },
                 ...messages,
             ],
